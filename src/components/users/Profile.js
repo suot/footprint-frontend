ff13760a5727ee39dd880410a51a4dbde999caf6
hongshuidang <tiansuo@live.com>
2019-02-22 21:38:55 -0500

    Fixed bug: sometimes current profile could not be loaded from firebase when profile component is rendered. Root cause: synchronizing profile from firebase takes time, even longer than profile component is rendered with null state.

--- src/components/users/Profile.js
+++ src/components/users/Profile.js
@@ -42,17 +42,43 @@ class Profile extends Component {
 
   constructor(props) {
     super(props);
+
+    //const profile = this.props.profile;
+    const auth = this.props.auth;
+
     this.state = {
-      uid: props.auth.uid,
-      email: props.auth.email,
-      firstName: props.profile.firstName,
-      lastName: props.profile.lastName,
-      gender: props.profile.gender,
-      personalStatus: props.profile.personalStatus,
-      avatar: props.profile.avatar,
+      // firstName: profile.firstName,
+      // lastName: profile.lastName,
+      // gender: profile.gender,
+      // personalStatus: profile.personalStatus,
+      // avatar: profile.avatar,
+
+      firstName: "",
+      lastName: "",
+      gender: "",
+      personalStatus: "",
+      avatar: "",
+      uid: auth.uid,
+      email: auth.email,
     };
 
   }
+
+
+  componentDidUpdate(prevProps, prevState) {
+    //synchronizing profile from firebase cloud database costs time.
+    if(prevProps.profile.isEmpty && (!this.props.profile.isEmpty)){
+      this.setState({
+        firstName: this.props.profile.firstName,
+        lastName: this.props.profile.lastName,
+        gender: this.props.profile.gender,
+        personalStatus: this.props.profile.personalStatus,
+        avatar: this.props.profile.avatar,
+      });
+    }
+  }
+
+
 
   updateInput = e => {
     this.setState({
@@ -68,89 +94,97 @@ class Profile extends Component {
 
   handleSubmit = () => {
     this.props.updateProfile(this.state);
-    //console.log(this.state);
-    //console.log(this.props.profile);
+    this.props.history.push('/');
   }
 
 
   render() {
-    return (
-      <div className="animated fadeIn mt-3">
-        <Row>
-          <Col xs="12" md="12">
-            <Card>
-              <CardHeader>
-                <strong>Edit your profile</strong>
-              </CardHeader>
-              <CardBody>
-                <FormGroup row className="my-0">
-                  <Col xs="2">
-                    <MyAvatarEditor transmitAvatar={this.transmitAvatar}/>
-                  </Col>
-                  <Col xs="10">
+    if(!this.props.profile.isEmpty) {
+      return (
+          <div className="animated fadeIn mt-3">
+            <Row>
+              <Col xs="12" md="12">
+                <Card>
+                  <CardHeader>
+                    <strong>Edit your profile</strong>
+                  </CardHeader>
+                  <CardBody>
                     <FormGroup row className="my-0">
-                      <Col xs="12">
-                        <FormGroup>
-                          <Label htmlFor="email">Email</Label>
-                          <Input type="text" id="email" value={this.state.email} disabled/>
-                        </FormGroup>
+                      <Col xs="2">
+                        <MyAvatarEditor transmitAvatar={this.transmitAvatar}/>
                       </Col>
-                    </FormGroup>
+                      <Col xs="10">
+                        <FormGroup row className="my-0">
+                          <Col xs="12">
+                            <FormGroup>
+                              <Label htmlFor="email">Email</Label>
+                              <Input type="text" id="email" value={this.state.email} disabled/>
+                            </FormGroup>
+                          </Col>
+                        </FormGroup>
 
-                    <FormGroup row className="my-0">
-                      <Col xs="6">
-                        <FormGroup>
-                          <Label htmlFor="firstName">First name</Label>
-                          <Input type="text" id="firstName" placeholder="Enter your first name" value={this.state.firstName} onChange={this.updateInput} />
+                        <FormGroup row className="my-0">
+                          <Col xs="6">
+                            <FormGroup>
+                              <Label htmlFor="firstName">First name</Label>
+                              <Input type="text" id="firstName" placeholder="Enter your first name"
+                                     value={this.state.firstName} onChange={this.updateInput}/>
+                            </FormGroup>
+                          </Col>
+                          <Col xs="6">
+                            <FormGroup>
+                              <Label htmlFor="lastName">Last name</Label>
+                              <Input type="text" id="lastName" placeholder="Enter your last name"
+                                     value={this.state.lastName} onChange={this.updateInput}/>
+                            </FormGroup>
+                          </Col>
                         </FormGroup>
-                      </Col>
-                      <Col xs="6">
-                        <FormGroup>
-                          <Label htmlFor="lastName">Last name</Label>
-                          <Input type="text" id="lastName" placeholder="Enter your last name" value={this.state.lastName} onChange={this.updateInput} />
-                        </FormGroup>
-                      </Col>
-                    </FormGroup>
 
-                    <FormGroup row className="my-0">
-                      <Col xs="6">
-                        <FormGroup>
-                          <Label htmlFor="gender">Gender</Label>
-                          <Input type="select" id="gender" value={this.state.gender} onChange={this.updateInput} >
-                            <option>Please select</option>
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                            <option value="Other">Other</option>
-                          </Input>
-                        </FormGroup>
-                      </Col>
-                      <Col xs="6">
-                        <FormGroup>
-                          <Label htmlFor="marriage">Personal status</Label>
-                          <Input type="select" id="personalStatus" value={this.state.personalStatus} onChange={this.updateInput} >
-                            <option>Please select</option>
-                            <option value="Single">Single</option>
-                            <option value="Coupled without kid">Coupled without kid</option>
-                            <option value="Coupled with kid">Coupled with kid</option>
-                          </Input>
+                        <FormGroup row className="my-0">
+                          <Col xs="6">
+                            <FormGroup>
+                              <Label htmlFor="gender">Gender</Label>
+                              <Input type="select" id="gender" value={this.state.gender} onChange={this.updateInput}>
+                                <option>Please select</option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                                <option value="Other">Other</option>
+                              </Input>
+                            </FormGroup>
+                          </Col>
+                          <Col xs="6">
+                            <FormGroup>
+                              <Label htmlFor="marriage">Personal status</Label>
+                              <Input type="select" id="personalStatus" value={this.state.personalStatus}
+                                     onChange={this.updateInput}>
+                                <option>Please select</option>
+                                <option value="Single">Single</option>
+                                <option value="Coupled without kid">Coupled without kid</option>
+                                <option value="Coupled with kid">Coupled with kid</option>
+                              </Input>
+                            </FormGroup>
+                          </Col>
                         </FormGroup>
                       </Col>
                     </FormGroup>
-                  </Col>
-                </FormGroup>
-              </CardBody>
-              <CardFooter>
-                <Button type="submit" color="primary" className="mr-2" onClick={this.handleSubmit}><i className="fa fa-dot-circle-o"></i> Submit</Button>
-                <Button type="reset" color="danger"><i className="fa fa-ban"></i> Reset</Button>
-              </CardFooter>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    );
+                  </CardBody>
+                  <CardFooter>
+                    <Button type="submit" color="primary" className="mr-2" onClick={this.handleSubmit}><i
+                        className="fa fa-dot-circle-o"></i> Submit</Button>
+                    <Button type="reset" color="danger"><i className="fa fa-ban"></i> Reset</Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+      );
+    }else{
+      return (
+          <div className="animated fadeIn pt-1 text-center">Loading profile...</div>
+      );
+    }
   }
 }
-
 
 
 
