a8e2202733bce1a2290f698ec1035b520d2cb710
hongshuidang <tiansuo@live.com>
2019-03-13 00:49:07 -0400

    Added Leaflet Map, BaseMap, and Marker with popup.

--- src/components/footprints/FootprintDetails.js
+++ src/components/footprints/FootprintDetails.js
@@ -6,57 +6,57 @@ import { connect } from 'react-redux'
 const FootprintDetails = ({footprint}) => {
     if(footprint){
         return(
-                <div className="animated fadeIn mt-3">
-                    <Row>
-                        <Col xs="12" md="12">
-                            <span><strong>A footprint: </strong></span>
-                            <FormGroup row className="my-0">
-                                <Col xs="6">
-                                    <FormGroup>
-                                        <Label>Country</Label>
-                                        <Input type="text" value={footprint.country} readOnly />
-                                    </FormGroup>
-                                </Col>
-                                <Col xs="6">
-                                    <FormGroup>
-                                        <Label>City</Label>
-                                        <Input type="text" value={footprint.city} readOnly />
-                                    </FormGroup>
-                                </Col>
-                            </FormGroup>
+            <div className="animated fadeIn mt-3">
+                <Row>
+                    <Col xs="12" md="12">
+                        <span><strong>A footprint: </strong></span>
+                        <FormGroup row className="my-0">
+                            <Col xs="6">
+                                <FormGroup>
+                                    <Label>Country</Label>
+                                    <Input type="text" value={footprint.country} readOnly />
+                                </FormGroup>
+                            </Col>
+                            <Col xs="6">
+                                <FormGroup>
+                                    <Label>City</Label>
+                                    <Input type="text" value={footprint.city} readOnly />
+                                </FormGroup>
+                            </Col>
+                        </FormGroup>
 
-                            <FormGroup row className="my-0">
-                                <Col xs="6">
-                                    <FormGroup>
-                                        <Label>Start</Label>
-                                        <Input type="text" value={footprint.startDate} readOnly />
-                                    </FormGroup>
-                                </Col>
-                                <Col xs="6">
-                                    <FormGroup>
-                                        <Label>End</Label>
-                                        <Input type="text" value={footprint.endDate} readOnly />
-                                    </FormGroup>
-                                </Col>
-                            </FormGroup>
+                        <FormGroup row className="my-0">
+                            <Col xs="6">
+                                <FormGroup>
+                                    <Label>Start</Label>
+                                    <Input type="text" value={footprint.startDate} readOnly />
+                                </FormGroup>
+                            </Col>
+                            <Col xs="6">
+                                <FormGroup>
+                                    <Label>End</Label>
+                                    <Input type="text" value={footprint.endDate} readOnly />
+                                </FormGroup>
+                            </Col>
+                        </FormGroup>
 
-                            <FormGroup row className="my-0">
-                                <Col xs="6">
-                                    <FormGroup>
-                                        <Label>Cost</Label>
-                                        <Input type="text" value={footprint.cost} readOnly />
-                                    </FormGroup>
-                                </Col>
-                                <Col xs="6">
-                                    <FormGroup>
-                                        <Label>Rating</Label>
-                                        <Input type="text" value={footprint.rating} readOnly />
-                                    </FormGroup>
-                                </Col>
-                            </FormGroup>
-                        </Col>
-                    </Row>
-                </div>
+                        <FormGroup row className="my-0">
+                            <Col xs="6">
+                                <FormGroup>
+                                    <Label>Cost</Label>
+                                    <Input type="text" value={footprint.cost} readOnly />
+                                </FormGroup>
+                            </Col>
+                            <Col xs="6">
+                                <FormGroup>
+                                    <Label>Rating</Label>
+                                    <Input type="text" value={footprint.rating} readOnly />
+                                </FormGroup>
+                            </Col>
+                        </FormGroup>
+                    </Col>
+                </Row>
+            </div>
         )
     }else{
         return (
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-
 const Dashboard = React.lazy(() => import('./components/dashboard/Dashboard'));
 const Profile = React.lazy(() => import('./components/users/Profile'));
 const AddFootprint = React.lazy(() => import('./components/footprints/AddFootprint'));
-const FootprintDetails = React.lazy(() => import('./components/footprints/FootprintDetails'));
+// const FootprintDetails = React.lazy(() => import('./components/footprints/FootprintDetails'));
 const Notfound = React.lazy(() => import('./components/auth/Notfound'));
 const Notifications = React.lazy(() => import('./components/dashboard/Notifications'));
+const MyFootprints = React.lazy(() => import('./components/map/MyFootprints'));
 
 
 const routes = [
@@ -14,7 +14,9 @@ const routes = [
   { path: '/profile', name: 'Profile', component: Profile },
   { path: '/notifications', name: 'Notifications', component: Notifications },
   { path: '/footprint/add', name: 'AddFootprint', component: AddFootprint },
-  { path: '/footprint/:id', name: 'FootprintDetailsById', component: FootprintDetails },
+  // { path: '/footprint/:id', name: 'FootprintDetailsById', component: FootprintDetails },
+
+  { path: '/footprint/:uid', name: 'FootprintsByUid', component: MyFootprints },
   { name: 'NotFound', component: Notfound },
 ];
 
